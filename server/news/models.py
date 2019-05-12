2a117ea62dcdf90d1bf76299e6d95846e0f967ae
Bruno L. Carli <brunolcarli@gmail.com>
2019-05-12 15:22:08 -0300

    ADD author to news model

--- server/news/models.py
+++ server/news/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 
 class News(models.Model):
     '''
@@ -7,5 +7,11 @@ class News(models.Model):
     '''
     title = models.CharField(max_length=100, null=False, blank=False)
     body = models.TextField(null=False, blank=False)
-    votes = models.IntegerField()
+    votes = models.IntegerField(default=0)
     publication_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.PROTECT
+        )
+
+
