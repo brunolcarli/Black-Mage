8461ca912203d3f033c3498381f3d9604fd76f31
Bruno L. Carli <brunolcarli@gmail.com>
2019-05-12 16:21:57 -0300

    ADD delete news

--- server/news/schema.py
+++ server/news/schema.py
@@ -143,6 +143,41 @@ class UpdateNews(graphene.relay.ClientIDMutation):
 
         return UpdateNews(news)
 
+
+class DeleteNews(graphene.relay.ClientIDMutation):
+    '''
+    Remove uma Notícia.
+    '''
+    news = graphene.Field(
+        NewsType,
+        description='Deleted News.'
+    )
+
+    class Input:
+        id = graphene.ID(description='ID da notícia', required=True)
+
+    @access_required
+    def mutate_and_get_payload(self, info, **_input):
+        _, id = from_global_id(_input.get('id'))
+        # identifica o usuario
+        user = info.context.user
+
+        # recupera o objeto do banco de dados
+        try:
+            news = News.objects.get(id=id)
+        except Exception as exception:
+            raise(exception)
+
+        # somente poderá modificar o objeto se for o autor do mesmo
+        if not news.author.id == user.id:
+            raise Exception("You don't have permission to do this.")
+
+        deleted_data = news
+        news.delete()
+        return DeleteNews(deleted_data)
+
+
 class Mutation:
     create_news = CreateNews.Field()
     update_news = UpdateNews.Field()
+    delete_news = DeleteNews.Field()
