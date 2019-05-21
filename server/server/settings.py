4fccec2f9384c7e7082a106acb68517c77f68672
Bruno L. Carli <brunolcarli@gmail.com>
2019-05-21 17:47:07 -0300

    ADD perfumaria

--- server/server/settings.py
+++ server/server/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from server.black_mage import teleport
+
+
+print(teleport)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
