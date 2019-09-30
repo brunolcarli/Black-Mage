d06014002d4a895d28e0da6c3530d83c123567a7
Bruno Luvizotto Carli <bruno.carli@smartgreen.net>
2019-09-30 18:15:26 -0300

    CHANGE split settings

--- server/settings/common.py
+++ server/settings/common.py
@@ -23,11 +23,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3_g^agzad+5*_yo-&p(@=!@=iq(5k=2sn)omzccotr@4r=p-j)'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
